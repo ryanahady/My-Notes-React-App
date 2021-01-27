3f15597bca1625bb11aefaaf688b49b230cf005f
Ryan A <ryan.zad@gmail.com>
2021-01-27 14:39:55 +1100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NoteAddIcon from "@material-ui/icons/NoteAdd";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NoteAddIcon />
+        React Notes App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
